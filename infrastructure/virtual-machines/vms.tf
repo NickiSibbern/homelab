1bc94ef8cf74baaa2e59730dc49f6e869fc8b67f
Nicki Sibbern <5722216+NickiSibbern@users.noreply.github.com>
2025-10-21 20:16:42 +0200

    Added longhorn to cluster and setup UI httproute

--- infrastructure/virtual-machines/vms.tf
+++ infrastructure/virtual-machines/vms.tf
@@ -37,7 +37,7 @@ systemctl restart containerd
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet kubeadm kubectl open-iscsi nfs-common cryptsetup dmsetup
 apt-mark hold kubelet kubeadm kubectl
 systemctl enable --now kubelet
 EOF
